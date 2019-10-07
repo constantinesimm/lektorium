a54ab8e5c37a15abe4c36abc60570601377effb6
Ringostat-Simm <simm.ringostat@gmail.com>
2019-10-08 01:32:24 +0300

    finished js task. delete tests

--- hometask-01/js/index.js
+++ hometask-01/js/index.js
@@ -1,104 +1,75 @@
 /*
-Дан масив чисел (положительных, отрицательных и в перемешку)
-Найти max, min, sum
-Залить на github решение
-Нельзя использовать методы массива, а только циклы for, while*
-Примеры массивов:
-[3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1]
-[-1,-8,-2]
-[1,7,3]
-[1,undefined,3,5,-3]
-[1,NaN,3,5,-3]
+    Дан масив чисел (положительных, отрицательных и в перемешку)
+    Найти max, min, sum
+    Залить на github решение
+    Нельзя использовать методы массива, а только циклы for, while*
+    Примеры массивов:
+    [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1]
+    [-1,-8,-2]
+    [1,7,3]
+    [1,undefined,3,5,-3]
+    [1,NaN,3,5,-3]
  */
 
-//helpers
-const helpers = {
-    generateRandomIntArray(firstValue, lastValue, arrayLength) {
-        let generatedArray = [],
-            rangeStartValue = Math.min(firstValue, lastValue), //get range start value
-            rangeEndValue = Math.max(firstValue, lastValue);   //get range end value
-        //generate random int array
-        while (generatedArray.length < arrayLength) {
-            generatedArray.push(Math.floor(Math.random() * (rangeEndValue - rangeStartValue) + rangeStartValue));
-        };
+const checkArgument = {
+    _isArray(array) {
+        if (!(array instanceof Array)) {
+            throw new TypeError(`Argument is not Array`);
+        }
+        if (!array.length) {
+            throw new Error(`Array is empty`);
+        }
 
-        return generatedArray;
+        return array;
     },
-    validateArray(source) {
-        let checkInstance = source instanceof Array,
-            checkLength = source.length;
-        //check source data is instance of array
-        if (!checkInstance) {
-            throw new TypeError('argument is not instance of Array. Check your source data')
-        }
-        //check source data array is not empty
-        if (!checkLength) {
-            throw new Error('Array can not be empty. Please check your source data')
+    _isNaN(index, value) {
+        if (isNaN(value)) {
+            throw new TypeError(`Array value at index ${index} isNaN – ${value}`);
         }
 
-        return source;
-    },
-    validateArrayIndex(indexValue) {
-        if (isNaN(indexValue)) {
-            throw new TypeError('one of arr values not-a-number')
-        }
-
-        return Number(indexValue)
+        return Number(value)
     }
 };
 
-function getArrayMinValue(sourceArray) {
-    let validArray = helpers.validateArray(sourceArray), //validate source arr or throw type error
-        minValue = validArray[0],                        //set default min value as arr first index
-        currentIterateIndex;                             //current iterate index value
+function findArrayMinValue(array) {
+    array = checkArgument._isArray(array);  //throw error if argument not array
+    let target = array[0],                  //set min value
+        currentIterateValue;                //current iterate value
 
-    for (let index = 0; index < validArray.length; index++) {
-        currentIterateIndex = helpers.validateArrayIndex(validArray[index]); //throw type error if NaN
-        if (currentIterateIndex < minValue) {
-            minValue = currentIterateIndex
+    for (let index = 0; index < array.length; index++) {
+        currentIterateValue = checkArgument._isNaN(index, array[index]);    //throw error if value isNaN
+        if (currentIterateValue < target) {
+            target = currentIterateValue;
         }
     }
 
-    return minValue;
+    return target;
 };
 
-function getArrayMaxValue(sourceArray) {
-    let validArray = helpers.validateArray(sourceArray), //validate source arr or throw type error
-        maxValue = validArray[0],                        //set default min value as arr first index
-        currentIterateIndex;                             //current iterate index value
+function findArrayMaxValue(array) {
+    array = checkArgument._isArray(array);  //throw error if argument not array
+    let target = array[0],                  //set max value
+        currentIterateValue;                //current iterate value
 
-    for (let index = 0; index < validArray.length; index++) {
-        currentIterateIndex = helpers.validateArrayIndex(validArray[index]); //throw type error if NaN
-        if (currentIterateIndex > maxValue) {
-            maxValue = currentIterateIndex
+    for (let index = 0; index < array.length; index++) {
+        currentIterateValue = checkArgument._isNaN(index, array[index]);    //throw error if value isNaN
+        if (currentIterateValue > target) {
+            target = currentIterateValue;
         }
     }
 
-    return maxValue;
+    return target;
 };
 
-function getArrayValuesSummary(sourceArray) {
-    let validArray = helpers.validateArray(sourceArray), //validate source arr or throw type error
-        summaryArrayValues = 0,                          //variable for array values summary
-        currentIterateIndex;                             //current iterate index value
+function summaryArrayValues(array) {
+    array = checkArgument._isArray(array);      //throw error if argument not array
+    let summary = null,                         //set default summary
+        currentIterateValue;                    //current iterate value
 
-    for (let index = 0; index < validArray.length; index++) {
-        currentIterateIndex = helpers.validateArrayIndex(validArray[index]); //throw type error if NaN
-        summaryArrayValues += currentIterateIndex;
+    for (let index = 0; index < array.length; index++) {
+        currentIterateValue = checkArgument._isNaN(index, array[index]);    //throw error if value isNaN
+        summary += currentIterateValue;
     }
 
-    return summaryArrayValues;
+    return summary;
 };
-
-//checking for result or errors
-let positiveNumbersArray = helpers.generateRandomIntArray(100, 1, 15),
-    negativeNumbersArray = helpers.generateRandomIntArray(-26, -176, 15),
-    mixedNumbersArray = helpers.generateRandomIntArray(-289, 545, 15),
-    notArray = {},
-    arrayWithNaN = [1,undefined,3,5,-13];
-
-console.log(`min value of positive nums – ${getArrayMinValue(positiveNumbersArray)}`);
-console.log(`max value of negative nums – ${getArrayMaxValue(negativeNumbersArray)}`);
-console.log(`sum of mixed nums – ${getArrayValuesSummary(mixedNumbersArray)}`);
-console.log(`error if not array – ${getArrayValuesSummary(notArray)}`);
-console.log(`error if NaN – ${getArrayValuesSummary(arrayWithNaN)}`);
