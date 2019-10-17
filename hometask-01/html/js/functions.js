00e39134002b5caaedb3671d4b34040bff9c5b17
Ringostat-Simm <simm.ringostat@gmail.com>
2019-10-17 23:07:46 +0300

    html add tags and attributes for all landing

--- hometask-01/html/js/functions.js
+++ hometask-01/html/js/functions.js
@@ -3,3 +3,7 @@ navToggler.onclick = function() {
     this.classList.toggle('active');
     document.querySelector(this.dataset.target).classList.toggle('active');
 }
+
+
+// remove after
+const percentFrom = (part, full) => ((part / full) * 100).toFixed(2);
