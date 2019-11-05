3cca6cd0e67abca633f0471541e5efa95a26f0e9
Ringostat-Simm <simm.ringostat@gmail.com>
2019-11-05 21:16:46 +0200

    fixed array methods

--- hometask-04/task03.js
+++ hometask-04/task03.js
@@ -5,13 +5,16 @@
 
  */
 
-Array.prototype.myEach = function(callback, scope) {
+Array.prototype.myEach = function (callback, scope) {
     for (let i = 0; i < this.length; i++) {
         callback.call(scope, this[i], i, this);
     }
 };
 
-Array.prototype.myMap = function(callback, scope) {
+Array.prototype.myMap = function (callback, scope) {
+    if (!callback) {
+        throw new SyntaxError('This prototype must have a second argument - callback function.');
+    }
     let array = [];
 
     for (let i = 0; i < this.length; i++) {
@@ -21,6 +24,24 @@ Array.prototype.myMap = function(callback, scope) {
     return array;
 };
 
-Array.prototype.mySort = function() {
-
+Array.prototype.mySort = function (compareFn) {
+    for (let i = 0; i < this.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < this.length; j++) {
+            if (compareFn) {
+                let comparison = compareFn(this[j], this[min]);
+                if (comparison < 0) {
+                    min = j;
+                }
+            } else if (this[j] < this[min]) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+            let tmp = this[i];
+            this[i] = this[min];
+            this[min] = tmp;
+        }
+    }
+    return this;
 };
