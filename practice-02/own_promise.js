ed00fd3e96c16d26e645410b8d2ba03bedd60871
Constantine Simm <constantine.peschanov@gmail.com>
2019-11-14 20:14:32 +0200

    finished second practice

---
+++ practice-02/own_promise.js
@@ -0,0 +1,24 @@
+function chain(...args) {
+    let fn = args.pop();                    //get last argument that is a func
+    let fnResult = fn.apply(this, args);    //get last arg func result with current arguments
+
+    return { then: function(...args1) {     //return then method
+            let cb = args1.pop();           //get this step last argument that is func
+            return chain.apply(this, [...args1, fnResult, cb])  //scope with current args, function result and new func
+        }}
+}
+
+
+//test
+chain('20', function(value) {
+    console.log(value);
+    let param = 1;
+    console.log(param);
+    return param;
+}).then('a', 'b', function(a, b, param) {
+    console.log(++param);
+    return param;
+}).then(function(param) {
+    console.log(++param);
+    return param;
+})
